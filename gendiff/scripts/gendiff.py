82b64c04abbce3bebad04fcc5a62539370f8bc41
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-04 19:07:42 +0300

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,11 +7,12 @@ from gendiff.modules.gendiff_func import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(
-                        prog='gendiff',
-                        description='Compares two configuration files and shows a difference.')
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
+    parser.add_argument('-f', '--format', metavar='FORMAT',
+                        help='set format of output')
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file))
 
