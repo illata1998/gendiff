702ec85a5148bf6b8ca81ecb8c5066738bd463d1
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-05-04 19:50:34 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,8 +6,8 @@ def test_generate_diff():
     file_path2 = 'tests/fixtures/file2.json'
     result_file1 = open('tests/fixtures/result1.txt')
     result_file2 = open('tests/fixtures/result2.txt')
-    result1 = result_file1.read()
-    result2 = result_file2.read()
+    result1 = result_file1.read()[:-1]
+    result2 = result_file2.read()[:-1]
     result_file1.close()
     result_file2.close()
     assert result1 == generate_diff(file_path1, file_path2)
