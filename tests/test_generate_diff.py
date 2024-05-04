f10ed881ac12a210c1b7c90016d3427904833ab3
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-04 19:40:12 +0300

    Add tests/

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,19 @@
+from gendiff.modules.gendiff_func import generate_diff
+
+
+def test_generate_diff():
+    file_path1 = 'fixtures/file1.json'
+    file_path2 = 'fixtures/file2.json'
+    file_path3 = 'fixtures/file3.json'
+    result_file1 = open('fixtures/result1')
+    result_file2 = open('fixtures/result2')
+    result_file3 = open('fixtures/result3')
+    result1 = result_file1.read()
+    result2 = result_file2.read()
+    result3 = result_file3.read()
+    result_file1.close()
+    result_file2.close()
+    result_file3.close()
+    assert result1 = generate_diff(file_path1, file_path2)
+    assert result2 = generate_diff(file_path1, file_path1)
+    assert result3 = generate_diff(file_path1, file_path3)
