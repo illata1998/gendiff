069f6b464b91548ae5ffac478194db9276f14d6a
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-05-05 17:02:43 +0300

    Update parse_func.py

--- gendiff/modules/parse_func.py
+++ gendiff/modules/parse_func.py
@@ -12,3 +12,18 @@ def parse_yaml(file_path):
     with open(file_path) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data
+
+
+def is_json(file_path):
+    return file_path.endswith('.json')
+
+
+def is_yaml(file_path):
+    file_path.endswith('.yaml') or file_path.endswith('.yml')
+
+
+def parse(file_path):
+    if is_json(file_path):
+        return parse_json(file_path)
+    elif is_yaml(file_path):
+        return parse_yaml(file_path)
