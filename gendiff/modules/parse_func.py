b398f936cdf0e00123198d39d7ed99ec9a93ffaa
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-05 16:28:07 +0300

    Create parse_func.py

---
+++ gendiff/modules/parse_func.py
@@ -0,0 +1,14 @@
+import json
+import yaml
+
+
+def parse_json(file_path):
+    with open(file_path) as f:
+        data = json.load(f)
+    return data
+
+
+def parse_yaml(file_path):
+    with open(file_path) as f:
+        data = yaml.load(f)
+    return data
