edeed31100fcce2cb4c8bfecb0b3b8baaebdca1e
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-05-05 17:02:21 +0300

    Update gendiff_func.py

--- gendiff/modules/gendiff_func.py
+++ gendiff/modules/gendiff_func.py
@@ -1,36 +1,10 @@
-import itertools
-from gendiff.modules.parse_func import parse_json, parse_yaml
-
-
-def stringify(value, replacer=' ', spaces_count=1):
-
-    def iter_(current_value, depth):
-        if not isinstance(current_value, dict):
-            if isinstance(current_value, bool):
-                return str(current_value).lower()
-            return str(current_value)
-
-        deep_indent_size = depth + spaces_count
-        deep_indent = replacer * deep_indent_size
-        current_indent = replacer * depth
-        lines = []
-        for key, val in current_value.items():
-            lines.append(f'{deep_indent}{key}: {iter_(val, deep_indent_size)}')
-        result = itertools.chain("{", lines, [current_indent + "}"])
-        return '\n'.join(result)
-
-    return iter_(value, 0)
+from gendiff.modules.stringify import stringify
+from gendiff.modules.parse_func import parse
 
 
 def generate_diff(file_path1, file_path2):
-    if file_path1.endswith('.json'):
-        data1 = parse_json(file_path1)
-    elif file_path1.endswith('.yaml') or file_path1.endswith('.yml'):
-        data1 = parse_yaml(file_path1)
-    if file_path2.endswith('.json'):
-        data2 = parse_json(file_path2)
-    elif file_path2.endswith('.yaml') or file_path2.endswith('.yml'):
-        data2 = parse_yaml(file_path2)
+    data1 = parse(file_path1)
+    data2 = parse(file_path2)
     result = {}
     for key in data1:
         if key in data2:
