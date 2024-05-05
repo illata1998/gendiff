d5e38bdff78c400de0554c73dab82512d9b8e5ba
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-05-05 16:50:12 +0300

    Update parse_func.py

--- gendiff/modules/parse_func.py
+++ gendiff/modules/parse_func.py
@@ -1,15 +1,14 @@
 import json
 import yaml
-from yaml.loader import SafeLoader
 
 
 def parse_json(file_path):
     with open(file_path) as f:
-        data = json.load(f, Loader=SafeLoader)
+        data = json.load(f, Loader=yaml.SafeLoader)
     return data
 
 
 def parse_yaml(file_path):
     with open(file_path) as f:
-        data = yaml.load(f, Loader=SafeLoader)
+        data = yaml.load(f, Loader=yaml.SafeLoader)
     return data
