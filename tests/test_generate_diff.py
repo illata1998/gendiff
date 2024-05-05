794410897b344d5f4282af8b07b49ff303a2c793
illata1998 <163180009+illata1998@users.noreply.github.com>
2024-05-05 16:34:58 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,13 +2,15 @@ from gendiff.modules.gendiff_func import generate_diff
 
 
 def test_generate_diff():
-    file_path1 = 'tests/fixtures/file1.json'
-    file_path2 = 'tests/fixtures/file2.json'
-    result_file1 = open('tests/fixtures/result1.txt')
-    result_file2 = open('tests/fixtures/result2.txt')
+    json_path1 = 'tests/fixtures/file1.json'
+    json_path2 = 'tests/fixtures/file2.json'
+    yaml_path1 = 'tests/fixtures/file1.yaml'
+    yaml_path2 = 'tests/fixtures/file2.yml'
+    result_file = open('tests/fixtures/result1.txt')
     result1 = result_file1.read()[:-1]
     result2 = result_file2.read()[:-1]
     result_file1.close()
     result_file2.close()
-    assert result1 == generate_diff(file_path1, file_path2)
-    assert result2 == generate_diff(file_path1, file_path1)
+    assert result == generate_diff(json_path1, json_path2)
+    assert result == generate_diff(yaml_path1, yaml_path2)
+    assert result == generate_diff(json_path1, yaml_path2)
