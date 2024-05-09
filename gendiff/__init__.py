384a5524badceea6eba3754796e069c3fa5b48f8
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-09 15:52:13 +0300

    Update __init__.py

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -2,7 +2,4 @@ from gendiff.modules.gendiff_func import generate_diff
 from gendiff.formatters.stylish import stylish
 
 
-__all__ = (
-           'generate_diff',
-           'stylish'
-           )
+__all__ = ('generate_diff', 'stylish')
