bcb22265d4cb8230718851e57b5a782439340c30
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-09 15:28:58 +0300

    Update gendiff_func.py

--- gendiff/modules/gendiff_func.py
+++ gendiff/modules/gendiff_func.py
@@ -1,22 +1,11 @@
-from gendiff.modules.stringify_func import stringify
 from gendiff.modules.parse_func import parse
+from gendiff.modules.diff import diff
+from gendiff.modules.choose_formatter import choose_formatter
 
 
-def generate_diff(file_path1, file_path2):
+def generate_diff(file_path1, file_path2, format='stylish'):
     data1 = parse(file_path1)
     data2 = parse(file_path2)
-    result = {}
-    for key in data1:
-        if key in data2:
-            if data1[key] == data2[key]:
-                result['  ' + key] = data1[key]
-            else:
-                result['- ' + key] = data1[key]
-                result['+ ' + key] = data2[key]
-        else:
-            result['- ' + key] = data1[key]
-    for key in data2:
-        if key not in data1:
-            result['+ ' + key] = data2[key]
-    result = dict(sorted(result.items(), key=lambda x: x[0][2:]))
-    return stringify(result)
+    formatter = choose_formatter(format)
+    return formatter(diff(data1, data2))
+
