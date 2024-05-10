c2f35b3980ebc479ae0a7999bdb7a2e3a5b4b19e
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-10 18:38:52 +0300

    Create cli.py

---
+++ gendiff/cli.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', default='stylish', metavar='FORMAT',
+                        help='set format of output')
+    return parser.parse_args()
