be546cb81a8136bd9f30c15e69a930debb63ea44
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-10 16:08:06 +0300

    Update choose_formatter.py

--- gendiff/modules/choose_formatter.py
+++ gendiff/modules/choose_formatter.py
@@ -1,7 +1,10 @@
 from gendiff.formatters.stylish import stylish
+from gendiff.formatters.plain import plain
 
 
 def choose_formatter(format):
     if format == 'stylish':
         func = stylish
+    elif format == 'plain':
+        func = plain
     return func
