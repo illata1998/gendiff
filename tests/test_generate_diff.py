ec594af54446a486990d4815f23f8ef6ce221ebb
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-05-13 20:39:14 +0300

    Update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,37 +1,25 @@
+import pytest
 from gendiff.modules.generate_diff import generate_diff
 
 
-def test_generate_diff_flat():
-    json_path1 = 'tests/fixtures/flat1.json'
-    json_path2 = 'tests/fixtures/flat2.json'
-    yaml_path1 = 'tests/fixtures/flat1.yaml'
-    yaml_path2 = 'tests/fixtures/flat2.yml'
-    with open('tests/fixtures/result_flat_stylish.txt') as result_stylish_file:
-        result_stylish = result_stylish_file.read()[:-1]
-    with open('tests/fixtures/result_flat_plain.txt') as result_plain_file:
-        result_plain = result_plain_file.read()[:-1]
-    with open('tests/fixtures/result_flat_json.txt') as result_json_file:
-        result_json = result_json_file.read()[:-1]
-    assert result_stylish == generate_diff(json_path1, json_path2)
-    assert result_stylish == generate_diff(yaml_path1, yaml_path2)
-    assert result_stylish == generate_diff(json_path1, yaml_path2)
-    assert result_plain == generate_diff(json_path1, json_path2, 'plain')
-    assert result_json == generate_diff(json_path1, json_path2, 'json')
+def read_results(result_path):
+    with open(result_path) as result_file:
+        return result_file.read()[:-1]
 
 
-def test_generate_diff_nested():
-    json_path1 = 'tests/fixtures/nested1.json'
-    json_path2 = 'tests/fixtures/nested2.json'
-    yaml_path1 = 'tests/fixtures/nested1.yaml'
-    yaml_path2 = 'tests/fixtures/nested2.yml'
-    with open('tests/fixtures/result_nested_stylish.txt') as result_stylish_file:
-        result_stylish = result_stylish_file.read()[:-1]
-    with open('tests/fixtures/result_nested_plain.txt') as result_plain_file:
-        result_plain = result_plain_file.read()[:-1]
-    with open('tests/fixtures/result_nested_json.txt') as result_json_file:
-        result_json = result_json_file.read()[:-1]
-    assert result_stylish == generate_diff(json_path1, json_path2)
-    assert result_stylish == generate_diff(yaml_path1, yaml_path2)
-    assert result_stylish == generate_diff(json_path1, yaml_path2)
-    assert result_plain == generate_diff(json_path1, json_path2, 'plain')
-    assert result_json == generate_diff(json_path1, json_path2, 'json')
+@pytest.mark.parametrize("result_path,formatter", [('tests/fixtures/result_flat_stylish.txt', 'stylish'),
+                                                   ('tests/fixtures/result_flat_plain.txt', 'plain'),
+                                                   ('tests/fixtures/result_flat_json.txt', 'json')])
+@pytest.mark.parametrize("file_path1", ['tests/fixtures/flat1.json', 'tests/fixtures/flat1.yaml'])
+@pytest.mark.parametrize("file_path2", ['tests/fixtures/flat2.json', 'tests/fixtures/flat2.yml'])
+def test_generate_diff_flat(result_path, file_path1, file_path2, formatter):
+    assert read_results(result_path) == generate_diff(file_path1, file_path2, format_name=formatter)
+
+
+@pytest.mark.parametrize("result_path,formatter", [('tests/fixtures/result_nested_stylish.txt', 'stylish'),
+                                                   ('tests/fixtures/result_nested_plain.txt', 'plain'),
+                                                   ('tests/fixtures/result_nested_json.txt', 'json')])
+@pytest.mark.parametrize("file_path1", ['tests/fixtures/nested1.json', 'tests/fixtures/nested1.yaml'])
+@pytest.mark.parametrize("file_path2", ['tests/fixtures/nested2.json', 'tests/fixtures/nested2.yml'])
+def test_generate_diff_nested(result_path, file_path1, file_path2, formatter):
+    assert read_results(result_path) == generate_diff(file_path1, file_path2, format_name=formatter)
